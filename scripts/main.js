0e63a1cc468c596bce81d36fb5499253153c8367
gulshat1705 <arkytbekovagulshat@gmail.com>
2025-01-05 17:28:51 +0600

    feat: resources page

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
+import ExpandableContentCollection from './ExpandableContent.js';
 import Header from './Header.js';
 import TabsCollection from './Tabs.js';
+import VideoPlayerCollection from './VideoPlayer.js';
 
 new Header();
 new TabsCollection();
+new VideoPlayerCollection();
+new ExpandableContentCollection();
